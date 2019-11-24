86cce6834c108555bb55310172a187a3b9aaeda1
Joji Varghese <jojiv@amazon.com>
2019-11-24 14:29:26 -0500

    comment app2 for app1

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -6,12 +6,12 @@ import { CdkFargateDockerECS } from '../lib/cdk-fargate-docker-ecs';
 import { CdkQueueLambda } from '../lib/cdk-queue-lambda';
 import { CdkQueueDynamoDb } from '../lib/cdk-queue-dynamoDb';
 
-// const app1 = new cdk.App();
-// new CdkFargateDockerECSPatterns(app1, 'CdkFargateDockerECSPatterns');
+const app1 = new cdk.App();
+new CdkFargateDockerECSPatterns(app1, 'CdkFargateDockerECSPatterns');
 // new CdkFargateDockerECS(app1, 'CdkFargateDockerECS');
 
-const app2 = new cdk.App();
-new CdkQueueDynamoDb(app2, 'CdkQueueDynamoDb');
+// const app2 = new cdk.App();
+// new CdkQueueDynamoDb(app2, 'CdkQueueDynamoDb');
 // new CdkQueueLambda(app2, 'CdkQueueLambda');
 
 // app2.synth();
